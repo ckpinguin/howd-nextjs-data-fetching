c5d70eca1f0223d3d9a138815833e6151e64923e
Bonnie Schulkin <biz@bonnieschulkin.com>
2024-06-11 19:34:01 -0700

    Solution for workshop 2

--- src/components/StyleGenerator/StyleGenerator.tsx
+++ src/components/StyleGenerator/StyleGenerator.tsx
@@ -2,10 +2,33 @@
 
 import React from "react";
 
+import Button from "../Button";
+import Card from "../Card";
+import Separator from "../Separator";
 import styles from "./StyleGenerator.module.css";
 
 function StyleGenerator() {
-  return <div className={styles.wrapper}>{/* where the magic happens */}</div>;
+  // `<string>` is for TypeScript, since
+  //    this state starts out as undefined
+  const [quote, setQuote] = React.useState<string>();
+
+  const handleClick = async () => {
+    const response = await fetch("/api/get-quote-style");
+    const json = await response.json();
+    // expecting JSON to have a `quotation` property,
+    // based on the route handler from last workshop
+    setQuote(json.quotation);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <Button onClick={handleClick}>use random quote</Button>
+      <Separator />
+      <Card textColor="aliceBlue" backgroundColor="mediumBlue">
+        {quote}
+      </Card>
+    </div>
+  );
 }
 
 export default StyleGenerator;
