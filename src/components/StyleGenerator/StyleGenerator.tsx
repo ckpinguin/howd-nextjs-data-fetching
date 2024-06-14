75bc46694f455ce402ddcb7628baa1050df4f225
Bonnie Schulkin <biz@bonnieschulkin.com>
2024-06-13 20:49:53 -0700

    Loading state

--- src/components/StyleGenerator/StyleGenerator.tsx
+++ src/components/StyleGenerator/StyleGenerator.tsx
@@ -7,17 +7,26 @@ import Card from "../Card";
 import Separator from "../Separator";
 import styles from "./StyleGenerator.module.css";
 
+type Status = "idle" | "loading" | "error";
+
 function StyleGenerator() {
-  // `<string>` is for TypeScript, since
-  //    this state starts out as undefined
   const [quote, setQuote] = React.useState<string>();
+  const [status, setStatus] = React.useState<Status>("idle");
+
+  console.log("STATUS", status);
 
   const handleClick = async () => {
+    // start request
+    setStatus("loading");
     const response = await fetch("/api/get-quote-style");
     const json = await response.json();
     // expecting JSON to have a `quote` property,
     // based on the route handler from last workshop
     setQuote(json.quote);
+
+    // set status back to idle
+    // after everything's settled
+    setStatus("idle");
   };
 
   return (
