ac8728280bf77f9a9b040d2b61aefaca548e2d72
Bonnie Schulkin <biz@bonnieschulkin.com>
2024-06-14 11:02:24 -0700

    Move to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,31 @@
 "use client";
 import React from "react";
 
+import Button from "@/components/Button";
+import Card from "@/components/Card";
+import Separator from "@/components/Separator";
+
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>{/* where the magic happens */}</main>;
+  // `<string>` is for TypeScript, since
+  //    this state starts out as undefined
+  const [quote, setQuote] = React.useState<string>();
+
+  const handleClick = async () => {
+    const response = await fetch("/api/get-quote-style");
+    const json = await response.json();
+    // expecting JSON to have a `quote` property,
+    // based on the route handler from last workshop
+    setQuote(json.quote);
+  };
+  return (
+    <main className={styles.main}>
+      <Button onClick={handleClick}>use random quote</Button>
+      <Separator />
+      <Card textColor="aliceBlue" backgroundColor="mediumBlue">
+        {quote}
+      </Card>
+    </main>
+  );
 }
