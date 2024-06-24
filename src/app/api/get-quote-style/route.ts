7b238a784936ef6072b8b0fa9a9b15f5c5d5b446
Bonnie Schulkin <biz@bonnieschulkin.com>
2024-06-24 15:35:27 -0700

    Update name of route

--- src/app/api/get-quote-style/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-import { getRandomQuote } from "@/helpers/random-quotes";
-
-export function GET() {
-  const generatedQuote = getRandomQuote();
-
-  return NextResponse.json({ quote: generatedQuote });
-}
