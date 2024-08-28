6b09e774f237c2174d3ba62d759e11dcf0361477
Christof Kälin <christof.kaelin@protonmail.com>
2024-08-28 11:46:36 +0200

    simplified by using NextResponse

--- src/app/api/get-quotes-styles/route.ts
+++ src/app/api/get-quotes-styles/route.ts
@@ -1,9 +1,9 @@
+import { NextResponse } from "next/server"
+
 import { getRandomQuote } from "@/helpers/random-quotes"
 
 export async function GET(request: Request) {
   const res = getRandomQuote()
   console.log(res)
-  return new Response(JSON.stringify(res), {
-    headers: { "Content-Type": "application/json" },
-  })
+  return NextResponse.json(res)
 }
