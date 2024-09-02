b1f41f6e35ee065961365fff6e43cb4a635a2f13
Christof Kälin <christof.kaelin@protonmail.com>
2024-09-02 09:35:22 +0200

    part 4 finished (made a lot of oversight-misunderstanding-errors)

--- src/app/api/get-quotes-styles/route.ts
+++ src/app/api/get-quotes-styles/route.ts
@@ -5,5 +5,8 @@ import { getRandomQuote } from "@/helpers/random-quotes"
 export async function GET(request: Request) {
   const res = getRandomQuote()
   console.log(res)
-  return NextResponse.json(JSON.stringify(res))
+  return NextResponse.json({ quote: res })
+  //return NextResponse.json({ res }) // wrong format, should error
+  //return NextResponse.error() // generic server error (500) response
+  //throw new Error("Not found") // thrown server error
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,23 +6,36 @@ type Status = "idle" | "loading" | "error"
 export default function Home() {
   const [quote, setQuote] = useState<string>()
   const [status, setStatus] = useState<Status>("idle")
+  const [error, setError] = useState<string>()
 
   console.log("STATUS", status)
   console.log("QUOTE", quote)
+  console.log("ERROR", error)
 
   const handleClick = async () => {
     setStatus("loading")
-    const response = await fetch("/api/get-quotes-styles")
-    const json = await response.json()
-    setQuote(json)
-    console.log(json)
-    setStatus("idle")
+    try {
+      const response = await fetch("/api/get-quotes-styles")
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      const json = await response.json()
+      if (!json?.quote) {
+        throw new Error("Malformed response")
+      }
+
+      setQuote(json.quote)
+      setStatus("idle")
+    } catch (err) {
+      setStatus("error")
+      setError(err?.toString())
+    }
   }
 
   return (
     <main>
       <button onClick={handleClick}>Get quote</button>
-      <div>{status === "loading" ? "loading" : quote}</div>
+      <div>{status === "loading" ? "loading" : JSON.stringify(quote)}</div>
     </main>
   )
 }
