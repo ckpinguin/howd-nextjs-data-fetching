3d72e4ec767412bfba53cbd7738834afd6e221b3
Christof Kälin <christof.kaelin@protonmail.com>
2024-09-08 21:08:42 +0200

    started next section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@
 import React from "react"
 
 import useQuoteStyles from "./hooks/useQuoteStyles"
+import Spinner from "@/components/Spinner"
 
 export default function Home() {
   const { fetchQuote, quote, status, error } = useQuoteStyles()
   return (
     <main>
       <button onClick={() => fetchQuote()}>Get quote</button>
-      <div>{status === "loading" ? "loading" : JSON.stringify(quote)}</div>
+      <div>{status === "loading" ? <Spinner /> : JSON.stringify(quote)}</div>
     </main>
   )
 }
---
+++ src/components/QuoteContent/QuoteContent.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+import styles from "./QuoteContent.module.css"
+
+export default function QuoteContent() {
+  return <div>QuoteContent</div>
+}
